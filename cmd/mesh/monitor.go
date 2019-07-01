e88b0648557a4dcfcb8870fbc9b465e6d1333381
Hasan <swept@protonmail.ch>
2019-07-01 19:54:50 +0100

    Added GetWishlist function

--- cmd/mesh/monitor.go
+++ cmd/mesh/monitor.go
@@ -3,11 +3,20 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
+	"net/http/cookiejar"
 	"net/url"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+var (
+	errInQueue = errors.New("Task in queue")
 )
 
 func (t *meshFrontendTask) Monitor() {
@@ -36,12 +45,44 @@ func (t *meshFrontendTask) SetProxy() {
 	}
 }
 
+func (t *meshFrontendTask) SetCookies() error {
+	jar, err := cookiejar.New(nil)
+
+	if err != nil {
+		return err
+	}
+
+	var cookies []*http.Cookie
+
+	for _, cookie := range t.SessionCookies {
+		cookies = append(cookies, cookie)
+	}
+
+	siteURL, err := url.Parse(t.Site.SiteURL)
+
+	if err != nil {
+		return err
+	}
+
+	jar.SetCookies(siteURL, cookies)
+
+	t.Client.Jar = jar
+
+	return nil
+}
+
 func (t *meshFrontendTask) GetSizes() (map[string]meshProductSKU, error) {
 
 	return nil, nil
 }
 
 func (t *meshFrontendTask) AddToWishlist() (bool, error) {
+	err := t.SetCookies()
+
+	if err != nil {
+		return false, err
+	}
+
 	wishlistPayload := &meshWishlistPayload{
 		Label:       nil,
 		IsPublic:    false,
@@ -80,7 +121,7 @@ func (t *meshFrontendTask) AddToWishlist() (bool, error) {
 	case 200:
 		if t.DetectQueue(resp.Cookies()) {
 			t.HandleQueue(req.URL.String())
-			return false, nil
+			return false, errInQueue
 		}
 
 		var response meshWishlistMessage
@@ -93,6 +134,13 @@ func (t *meshFrontendTask) AddToWishlist() (bool, error) {
 
 		if response.Message == "Wishlist updated successfully" {
 			log.Printf("[INFO] Item added to wishlist - %v - %v", t.SKU, t.SiteCode)
+
+			for _, cookie := range resp.Cookies() {
+				if cookie.Name == sessionCookie {
+					t.SessionCookies[sessionCookie] = cookie
+				}
+			}
+
 			return true, nil
 		}
 
@@ -109,8 +157,60 @@ func (t *meshFrontendTask) AddToWishlist() (bool, error) {
 	}
 }
 
-func (t *meshFrontendTask) GetWishlistID() {
+func (t *meshFrontendTask) GetWishlistID() (string, error) {
+	err := t.SetCookies()
 
+	if err != nil {
+		return "", err
+	}
+
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%v/wishlists/", t.Site.SiteURL), nil)
+
+	req.Header.Set("Pragma", "no-cache")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "*/*")
+	req.Header.Set("Accept-Language", "en-GB,en-US;q=0.9,en;q=0.8")
+	req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("X-Requested-With", "XMLHttpRequest")
+
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := t.Client.Do(req)
+
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	switch resp.StatusCode {
+	case 200:
+		if t.DetectQueue(resp.Cookies()) {
+			t.HandleQueue(req.URL.String())
+			return "", errInQueue
+		}
+
+		wishlistID, wishlistExists := doc.Find(fmt.Sprintf(`*[data-sku="%v%v"]`, t.SKU, t.Site.SKUSuffix)).Attr("data-wishlistid")
+
+		if wishlistExists {
+			log.Printf("[INFO] Found Wishlist - %v - %v - %v", wishlistID, t.SKU, t.SiteCode)
+			return wishlistID, nil
+		}
+
+		log.Printf("[WARN] No Wishlist - %v - %v", t.SKU, t.SiteCode)
+		return "", nil
+	default:
+		return "", fmt.Errorf("Invalid Status Code (Frontend - Wishlist) - %v - %v", t.SKU, t.SiteCode)
+	}
 }
 
 func (t *meshFrontendTask) DetectQueue(cookies []*http.Cookie) bool {
@@ -140,6 +240,8 @@ func (t *meshFrontendTask) HandleQueue(queueURL string) {
 
 func (t *meshFrontendTask) QueueBrute(queueURL string) (*http.Cookie, error) {
 	bruteClient := new(http.Client)
+
+	bruteClient.Timeout = 15 * time.Second
 
 	bruteClient.Transport = t.Client.Transport
 
@@ -179,7 +281,6 @@ func (t *meshFrontendTask) QueueBrute(queueURL string) (*http.Cookie, error) {
 	default:
 		return nil, fmt.Errorf("Unknown Status Code (Frontend - Queue Brute) - %v - %v", t.SKU, t.SiteCode)
 	}
-
 }
 
 func (t *meshBackendTask) Monitor() {
